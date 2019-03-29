aa1e0eaba223d42ee6244c9dcc0c344396516f74
Chris Richardson <chris@bpi.cam.ac.uk>
2019-03-29 12:06:48 +0000

    Fix flake8

--- python/test/unit/fem/test_element_doflayout.py
+++ python/test/unit/fem/test_element_doflayout.py
@@ -5,11 +5,8 @@
 # SPDX-License-Identifier:    LGPL-3.0-or-later
 """Unit tests for assembly over domains"""
 
-import numpy
-
 import dolfin
 from dolfin import cpp
-import ufl
 
 
 def test_element_dof_layout():
@@ -25,7 +22,7 @@ def test_element_dof_layout():
     cpp_dofmap = cpp.fem.DofMap(P1_layout, mesh)
     dofmap = dolfin.DofMap(cpp_dofmap)
     for i in range(mesh.num_entities(2)):
-        print (dofmap.cell_dofs(i))
+        print(dofmap.cell_dofs(i))
 
     # Vector layout, contiguous
     P1_x = cpp.fem.ElementDofLayout(1, [[[0], [1], [2]]],
@@ -46,20 +43,19 @@ def test_element_dof_layout():
                                      [[0, 1, 2]]],
                                     [6, 7, 8], [])
 
-
     Vector_layout = cpp.fem.ElementDofLayout(1, [[[0, 3, 6], [1, 4, 7], [2, 5, 8]]],
                                              [[[0, 3, 6], [1, 4, 7], [2, 5, 8]],
                                              [[1, 2, 4, 5, 7, 8],
                                               [0, 2, 3, 5, 6, 8],
                                               [0, 1, 3, 4, 6, 7]],
                                               [[0, 1, 2, 3, 4, 5, 6, 7, 8]]],
-                                              [], [P1_x, P1_y, P1_z])
+                                             [], [P1_x, P1_y, P1_z])
 
     cpp_dofmap = cpp.fem.DofMap(Vector_layout, mesh)
     dofmap = dolfin.DofMap(cpp_dofmap)
 
     for i in range(mesh.num_entities(2)):
-        print (dofmap.cell_dofs(i))
+        print(dofmap.cell_dofs(i))
 
     # Vector layout, interleaved
     P1_x = cpp.fem.ElementDofLayout(1, [[[0], [1], [2]]],
@@ -80,17 +76,16 @@ def test_element_dof_layout():
                                      [[0, 1, 2]]],
                                     [2, 5, 8], [])
 
-
     Vector_layout = cpp.fem.ElementDofLayout(1, [[[0, 1, 2], [3, 4, 5], [6, 7, 8]]],
                                              [[[0, 1, 2], [3, 4, 5], [6, 7, 8]],
                                              [[3, 4, 5, 6, 7, 8],
                                               [0, 1, 2, 6, 7, 8],
                                               [0, 1, 2, 3, 4, 5]],
                                               [[0, 1, 2, 3, 4, 5, 6, 7, 8]]],
-                                              [], [P1_x, P1_y, P1_z])
+                                             [], [P1_x, P1_y, P1_z])
 
     cpp_dofmap = cpp.fem.DofMap(Vector_layout, mesh)
     dofmap = dolfin.DofMap(cpp_dofmap)
 
     for i in range(mesh.num_entities(2)):
-        print (dofmap.cell_dofs(i))
+        print(dofmap.cell_dofs(i))
