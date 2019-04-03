544138385d3dfcc377fa1c6a795b6ea818a479e5
igorbaratta <igorbaratta@gmail.com>
2019-04-02 23:07:09 -0300

    start point source constructor

--- cpp/dolfin/fem/PointSource.cpp
+++ cpp/dolfin/fem/PointSource.cpp
@@ -7,6 +7,7 @@
 #include "PointSource.h"
 #include <dolfin/geometry/Point.h>
 #include <dolfin/function/FunctionSpace.h>
+#include <dolfin/la/PETScVector.h>
 
 
 using namespace dolfin;
@@ -14,11 +15,20 @@ using namespace dolfin;
 
 
 //-----------------------------------------------------------------------------
-fem::PointSource::PointSource(std::shared_ptr<const function::FunctionSpace> V, 
+fem::PointSource::PointSource(std::shared_ptr<function::FunctionSpace> V,
                               const geometry::Point& point,
-                                PetscScalar magnitude)
+                              PetscScalar magnitude): _V0(V)
 {
-
-
+    // Check if FunctionSpace is scalar
+    // FIXME: Allow vectoria Point Sources
+    assert(V->element()->space_dimension() == 1);
+    _point = point;
+    _magniude = magnitude;
 }
 //-----------------------------------------------------------------------------
+
+void fem::PointSource::apply(la::PETScVector& b)
+{
+    assert(b.size() > 0);
+
+}
--- cpp/dolfin/fem/PointSource.h
+++ cpp/dolfin/fem/PointSource.h
@@ -9,6 +9,7 @@
 #include <dolfin/geometry/Point.h>
 #include <petscsys.h>
 #include <memory>
+#include <utility>
 
 namespace dolfin
 {
@@ -20,7 +21,7 @@ class FunctionSpace;
 
 namespace la
 {
-class PetscVector;
+class PETScVector;
 }
 
 
@@ -31,14 +32,23 @@ class PointSource
 {
     public:
     /// Create point source at given point of given magnitude
-    PointSource(std::shared_ptr<const function::FunctionSpace> V,
+    PointSource(std::shared_ptr<function::FunctionSpace> V,
                 const geometry::Point& point,
                 PetscScalar magnitude);
     
+    /// Apply point source to right-hand side vector
+    void apply(la::PETScVector& b);
 
+    /// Destructor
+    ~PointSource() = default;
 
+    private:
+    // Source term - pair of points and magnitude
+    geometry::Point _point;
+    PetscScalar _magniude;
+    std::shared_ptr<function::FunctionSpace> _V0;
 };
 
-}
+} // namespace fem
+} // namespace dolfin
 
-}
